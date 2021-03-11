1b8ab47c4a2ea46b0443f1af96ef865c332b2e0f
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-11 11:53:16 -0300

    Criando classe de serviço do Cliente

---
+++ sistema-ecommerce/src/main/java/br/com/zup/sistemaecommerce/services/ClienteService.java
@@ -0,0 +1,49 @@
+package br.com.zup.sistemaecommerce.services;
+import br.com.zup.sistemaecommerce.models.Cliente;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Franklin Hanemann
+ * Classe que realiza todas as funcionalidades relacionadas a classe Cliente:
+ *      - Cadastrar Cliente (com validação de cliente com CPF ja cadastrado)
+ *      - Pesquisar Cliente pelo CPF
+ *      - Pesquisar Cliente
+ *
+ */
+@Service
+public class ClienteService {
+
+    private List<Cliente> listaClientes = new ArrayList<>();
+
+    public Cliente cadastrarCliente(Cliente cliente){
+        try {
+            pesquisarCliente(cliente);
+        } catch (RuntimeException erro){
+            listaClientes.add(cliente);
+            return cliente;
+        }
+        throw new RuntimeException("Cliente já cadastrado com o CPF "+cliente.getCpf());
+    }
+
+    public Cliente pesquisarClientePeloCPF(String cpf){
+        for (Cliente objetoCliente : listaClientes){
+            if (objetoCliente.getCpf().equalsIgnoreCase(cpf)){
+                return objetoCliente;
+            }
+        }
+
+        throw new RuntimeException("Cliente não encontrado!");
+    }
+
+    public Cliente pesquisarCliente(Cliente cliente){
+        return pesquisarClientePeloCPF(cliente.getCpf());
+    }
+
+    public List<Cliente> listarClientes(){
+        return listaClientes;
+    }
+}
+
