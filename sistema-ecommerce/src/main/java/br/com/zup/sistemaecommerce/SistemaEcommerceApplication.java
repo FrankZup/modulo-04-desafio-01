8397799325085725df9a4d51c7e08500e35539fc
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-11 11:32:27 -0300

    Iniciando o projeto

---
+++ sistema-ecommerce/src/main/java/br/com/zup/sistemaecommerce/SistemaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.sistemaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEcommerceApplication.class, args);
+	}
+
+}
