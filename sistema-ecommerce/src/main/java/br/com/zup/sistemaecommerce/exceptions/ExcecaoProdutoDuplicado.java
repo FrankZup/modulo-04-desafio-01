28e487ac39888d147dc15f32cf434ec733026651
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-11 12:49:51 -0300

    Criando classe de excessão para produto duplicado

---
+++ sistema-ecommerce/src/main/java/br/com/zup/sistemaecommerce/exceptions/ExcecaoProdutoDuplicado.java
@@ -0,0 +1,15 @@
+package br.com.zup.sistemaecommerce.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ExcecaoProdutoDuplicado extends Excecao {
+
+    public ExcecaoProdutoDuplicado() {
+        super("Cliente não cadastrado!", HttpStatus.BAD_REQUEST, "cliente");
+    }
+
+    public ExcecaoProdutoDuplicado(String nomeProduto) {
+        super("Produto "+nomeProduto+ " já cadastradp!", HttpStatus.BAD_REQUEST, "cliente");
+
+    }
+}
