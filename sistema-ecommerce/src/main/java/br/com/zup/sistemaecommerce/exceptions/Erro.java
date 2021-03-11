3d5d0d4d0035d65ac8873fee432bd52ca4ab0df2
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-11 12:38:53 -0300

    Criando classe Erro

---
+++ sistema-ecommerce/src/main/java/br/com/zup/sistemaecommerce/exceptions/Erro.java
@@ -0,0 +1,21 @@
+package br.com.zup.sistemaecommerce.exceptions;
+
+public class Erro {
+
+    private String campo;
+    private String mensagem;
+
+    public Erro() {
+    }
+
+    public Erro(String campo, String mensagem) {
+        this.campo    = campo ;
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() { return campo; }
+    public void setCampo(String campo) { this.campo = campo; }
+
+    public String getMensagem() { return mensagem; }
+    public void setMensagem(String mensagem) { this.mensagem = mensagem; }
+}
