15642ea834fd7a1a03a03b0c16df4eef96ed445e
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-11 18:35:51 -0300

    Corrigindo mensagem da exceção personalizada de compra não finalizada na classe de serviço de Compra

--- sistema-ecommerce/src/main/java/br/com/zup/sistemaecommerce/exceptions/ExcecaoCompraNaoFinalizada.java
+++ sistema-ecommerce/src/main/java/br/com/zup/sistemaecommerce/exceptions/ExcecaoCompraNaoFinalizada.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 public class ExcecaoCompraNaoFinalizada extends Excecao {
 
     public ExcecaoCompraNaoFinalizada() {
-        super("Cliente não finalizada!", HttpStatus.BAD_REQUEST, "cliente");
+        super("Compra não finalizada!", HttpStatus.BAD_REQUEST, "cliente");
     }
 }
