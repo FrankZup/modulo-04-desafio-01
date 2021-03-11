f0162948b81d1b71a59d84a1b4b69ca07f9c1c55
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-11 12:07:33 -0300

    Criando classe de serviço do Compra

---
+++ sistema-ecommerce/src/main/java/br/com/zup/sistemaecommerce/services/CompraService.java
@@ -0,0 +1,41 @@
+package br.com.zup.sistemaecommerce.services;
+
+import br.com.zup.sistemaecommerce.models.Compra;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Franklin Hanemann
+ *  Classe que realiza todas as funcionalidades relacionadas a classe Compra:
+ *        - Registrar Compra
+ *        - Listar todas as compras
+ *        - Pesquisar Compra pelo CPF do Cliente
+ */
+@Service
+public class CompraService {
+
+    private List<Compra> listaCompras = new ArrayList<>();
+
+    public Compra registrarCompra(Compra compra){
+        listaCompras.add(compra);
+        return compra;
+    }
+
+    public List<Compra> listarCompras(){
+        return listaCompras;
+    }
+
+    public List<Compra> pesquisarCompraPeloCPFdoCliente(String cpf){
+        for (Compra objetoCompra: listaCompras){
+            if (objetoCompra.getCliente().getCpf().equalsIgnoreCase(cpf)){
+                listaCompras.add(objetoCompra);
+            }
+        }
+
+        throw new RuntimeException("Compra não encontrada");
+    }
+
+}
